cadb43ee516e1dba29a086b968914f1c6fcd1c2b
jordan <jordan-billon@hotmail.fr>
2025-02-28 14:08:57 +0100

    exo fini

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
+import Combat.Assassin;
 import Combat.Guerrier;
 import Combat.Partie;
 import Combat.Personnage;
 
 public class Main {
     public static void main(String[] args) {
-        // Création des personnages
-        Guerrier guerrier = new Guerrier("Conan", 120, 40, 15, 20);
-        Personnage mage = new Personnage("Gandalf", 100, 30, 5);
 
-        // Création et lancement de la partie
-        Partie partie = new Partie(guerrier, mage, 1);
+        Guerrier guerrier = new Guerrier("Conan", 120, 40, 15, 20);
+        Assassin assassin = new Assassin("Rogue", 70, 60, 5, 20);
+
+        Partie partie = new Partie(guerrier, assassin, 1);
         String resultat = partie.lancerPartie();
 
-        // Affichage du résultat final
-        System.out.println("Résultat final : " + resultat);
     }
 }
